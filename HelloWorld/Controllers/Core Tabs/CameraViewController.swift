f5f79cc27716ba39c308b597b176a0d2ca686c8b
jbrown400 <justin.brown400@gmail.com>
2020-08-25 15:40:48 -0500

    Adding a bit of foundation to profile, notifications, and camera screens.

--- HelloWorld/Controllers/Core Tabs/CameraViewController.swift
+++ HelloWorld/Controllers/Core Tabs/CameraViewController.swift
@@ -1,30 +1,15 @@
-//
-//  CameraViewController.swift
-//  HelloWorld
-//
-//  Created by Justin Brown on 8/22/20.
-//  Copyright © 2020 Justin Brown. All rights reserved.
-//
-
+import AVFoundation
 import UIKit
 
 class CameraViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationController?.setNavigationBarHidden(true, animated: false)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func didTapTakePicture() {
+        
     }
-    */
 
 }
--- HelloWorld/Controllers/Core Tabs/ProfileViewController.swift
+++ HelloWorld/Controllers/Core Tabs/ProfileViewController.swift
@@ -1,19 +1,34 @@
-//
-//  ProfileViewController.swift
-//  HelloWorld
-//
-//  Created by Justin Brown on 8/22/20.
-//  Copyright © 2020 Justin Brown. All rights reserved.
-//
-
 import UIKit
 
-class ProfileViewController: UIViewController {
+/// Profile view controller
+final class ProfileViewController: UIViewController {
+    
+    private var collectionView: UICollectionView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         configureNviationBar()
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: view.width/3.0, height: view.width/3.0)
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView?.delegate = self
+        collectionView?.delegate = self
+        
+        guard let collectionView = collectionView else {
+            return
+        }
+        
+        view.addSubview(collectionView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView?.frame = view.bounds
     }
     
     private func configureNviationBar() {
@@ -28,4 +43,19 @@ class ProfileViewController: UIViewController {
         vc.title = "Settings"
         navigationController?.pushViewController(vc, animated: true)
     }
+}
+
+extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
+    
 }
