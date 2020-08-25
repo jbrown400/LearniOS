f1849748c3a0677e7803e19dfb5ccbbbb000417e
jbrown400 <justin.brown400@gmail.com>
2020-08-25 12:27:41 -0500

    Set up Firebase Database. Added basic username/email/password field validation. Added user creation/insertion functionality.

--- HelloWorld/Resources/AuthManager.swift
+++ HelloWorld/Resources/AuthManager.swift
@@ -12,10 +12,39 @@ import FirebaseAuth
 public class AuthManager {
     static let shared = AuthManager()
     
-    // Mark: - Public
-    
-    public func registerNewUser(username: String, email: String, password: String) {
+    public func registerNewUser(username: String, email: String, password: String, completion: @escaping (Bool) -> Void) {
+        /*
+         1. Check if username is available
+         2. Check if emial is available
+         3. Create account
+         4. Insert account to DB
+         */
         
+        DatabaseManager.shared.canCreateNewUser(with: email, username: username) { canCreate in
+            if canCreate {
+                Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+                    guard error == nil, result != nil else {
+                        // Firebase auth could note create account
+                        completion(false)
+                        return
+                    }
+                    
+                    // Insert into DB
+                    DatabaseManager.shared.insertNewUser(with: email, username: username) { inserted in
+                        if inserted {
+                            completion(true)
+                            return
+                        } else {
+                            completion(false)
+                            return
+                        }
+                    }
+                }
+            } else {
+                // either username or email does not exist
+                completion(false)
+            }
+        }
     }
     
     public func loginUser(username: String?, email: String?, password: String, completion: @escaping (Bool) -> Void) {
--- HelloWorld/Resources/DatabaseManager.swift
+++ HelloWorld/Resources/DatabaseManager.swift
@@ -10,9 +10,35 @@ import Foundation
 import FirebaseDatabase
 
 public class DatabaseManager {
+    
     static let shared = DatabaseManager()
     
-    // Mark: - Public
+    private let database = Database.database().reference()
+    
+    /// Check if username and email is available
+    /// - Parameters
+    ///     - email: String representing email
+    ///     - username: String representing username
+    public func canCreateNewUser(with email: String, username: String, completion: (Bool) -> Void) {
+        completion(true)
+    }
+    
+    /// Inserts new user data into database
+    /// - Parameters
+    ///     - email: String representing email
+    ///     - username: String representing username
+    ///     - completion: Async callback for result if database entry succeeded
+    public func insertNewUser(with email: String, username: String, completion: @escaping (Bool) -> Void) {
+        database.child(email.safeDatabaseKey()).setValue(["username": username]) { error, _ in
+            if error == nil {
+                completion(true)
+                return
+            } else {
+                completion(false)
+                return
+            }
+        }
+    }
     
     
 }
