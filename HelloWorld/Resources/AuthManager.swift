90c3258f07ec93444cedced98a7360524261bbe1
jbrown400 <justin.brown400@gmail.com>
2020-08-25 14:56:41 -0500

    Added log out functionality: confirming log out, redirect to login page.

--- HelloWorld/Resources/AuthManager.swift
+++ HelloWorld/Resources/AuthManager.swift
@@ -64,4 +64,16 @@ public class AuthManager {
             print(username)
         }
     }
+    
+    public func logOut(completion: (Bool) -> Void) {
+        do {
+            try Auth.auth().signOut()
+            completion(true)
+            return
+        } catch {
+            print(error)
+            completion(false)
+            return
+        }
+    }
 }
