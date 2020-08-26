e6bb19d1f3e7e0793622a5cfec542e2b2e787f62
jbrown400 <justin.brown400@gmail.com>
2020-08-26 12:01:15 -0500

    Stubbing table view cells we'll need for home screen. Added some functionality for uploading/downloading posts.

---
+++ HelloWorld/Views/Cells/HomeFeed/IGFeedPostActionsTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  IGFeedPostActionsTableViewCell.swift
+//  HelloWorld
+//
+//  Created by Justin Brown on 8/26/20.
+//  Copyright © 2020 Justin Brown. All rights reserved.
+//
+
+import UIKit
+
+class IGFeedPostActionsTableViewCell: UITableViewCell {
+
+
+
+}
